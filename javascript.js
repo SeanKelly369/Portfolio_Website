2be236e58e2c217f7ebbeccea49247b6a7aab019
Sean Kelly <36159246+SeanKelly369@users.noreply.github.com>
2018-07-28 11:18:33 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,99 @@
+let textFlag = true;
+
+function growShrinkNav() {
+  let Logo = document.getElementById("logo_header");
+  //console.log(Logo);
+  if (document.body.scrollTop > 90 || 			 document.documentElement.scrollTop > 20) {
+		textFlag = false;
+    Logo.style.width = '37px';
+		innerNav.style.marginTop = '.5%';
+    nav.style.height = '54px';
+    Logo.style.transition = 'all ease .5s';
+    nav_links.style.transition = 'all ease .5s';
+    nav_links.style.height = '38px';
+		innerNav.style.fontSize = '12px';
+		innerNav.style.transition = 'all easy .5s';
+  } else {
+    Logo.style.width = '62px';
+    Logo.style.padding = '2px';
+    nav.style.height = '80px';
+    nav_links.style.height = '60px';
+    nav_links.style.transition = 'all ease .5s';
+    Logo.style.transition = 'all ease .5s';
+		innerNav.style.marginTop = '1%';
+		innerNav.style.fontSize = '14px';
+		innerNav.style.transition = 'all ease .5s';
+		textFlag = true;
+  }
+}
+
+function navChange() {
+  if (window.outerWidth < 600 && window.outerWidth > 499) {
+		if(textFlag){
+			innerNav.style.fontSize = '10px';
+		}else if(textFlag = false){
+				innerNav.style.fontSize = '12px';
+		}
+    nav_links.style.width = '66%';
+    nav_links.style.marginRight = '60px';
+  } else if (window.outerWidth < 700 && window.outerWidth > 600) {
+    nav_links.style.width = '70%';
+    nav_links.style.marginRight = '80px';
+		if(textFlag) {
+			innerNav.style.fontSize = '12px';
+		}
+
+  } else if (window.outerWidth < 499) {
+    nav_links.style.width = '62%';
+    nav_links.style.marginRight = '30px';
+		if(textFlag){
+			innerNav.style.fontSize = '7px';
+		}else if(textFlag = false){
+				innerNav.style.fontSize = '12px';
+		}
+		// innerNav.style.fontSize = '7px';
+		innerNav.style.padding = '2px';
+
+  } else if (window.outerWidth < 800 && window.outerWidth > 700) {
+    nav_links.style.width = '72%';
+    nav_links.style.marginRight = '90px';
+
+  } else {
+    nav_links.style.width = '76%';
+    nav_links.style.marginRight = '100px';
+  }
+  // console.log(window.outerWidth);
+}
+
+//Open window showing details about the Sioux Watch
+function showWatch() {
+	let watchDetails = document.getElementById("watchDetails").style.display = 'block';
+	let overlayOpen = document.getElementById('overlay').style.display = 'block';
+	overlayOpen.style.transition = 'all ease .5s';
+}
+
+//Open window showing details about Biopodz
+function showBiopodz() {
+	let biopodzDetails = document.getElementById("biopodzDetails").style.display = 'block';
+	let overlayOpen = document.getElementById('overlay').style.display = 'block';
+	overlayOpen.style.transition = 'all ease .5s';
+}
+
+//Open window showing details about Biopodz
+function showEpee() {
+	let epeeDetails = document.getElementById("epeeDetails").style.display = 'block';
+	let overlayOpen = document.getElementById('overlay').style.display = 'block';
+	overlayOpen.style.transition = 'all ease .5s';
+}
+
+
+//Close window
+function hideProject() {
+	// let close = document.getElementById('overlay').style.display = 'none';
+	let closeWatch = document.getElementById('watchDetails').style.display = 'none';
+  document.getElementById("biopodzDetails").style.display = 'none';
+  document.getElementById("epeeDetails").style.display= 'none';
+  document.getElementById('overlay').style.display = 'none';
+	// close.style.transition = 'all ease .5s';
+  // closeWatch.style.transition = 'all ease .5s';
+}
